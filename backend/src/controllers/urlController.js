ab6ac7c5f9bd10c14e649b917f7b046545730555
Daniel Duarte <danielduartemora27@gmail.com>
2025-04-29 16:25:34 +0200

    feat: add core URL controller with shorten, redirect, and stats endpoints

--- backend/src/controllers/urlController.js
+++ backend/src/controllers/urlController.js
@@ -1,61 +1,50 @@
 import * as urlService from "../services/urlService.js";
-import { AppError } from "../utils/appError.js";
-import validUrl from "valid-url";
-export const shortenUrl = async (req, res, next) => {
-  try {
-    const { originalUrl } = req.body;
-    if (!originalUrl) {
-      return next(new AppError("originalUrl is required", 400));
-    }
+import { tryCatchFn } from "../utils/tryCatch.js";
+import { shortenUrlSchema } from "../schemas/urlSchema.js";
+import validate from "../middlewares/validationMiddleware.js";
 
-    if (!validUrl.isWebUri(originalUrl)) {
-      return next(new AppError("Invalid URL format", 400));
-    }
+export const shortenUrl = tryCatchFn(async (req, res, _next) => {
+  const { originalUrl } = req.validatedData.body;
+  const url = await urlService.createShortUrl(originalUrl);
+  res.status(201).json({
+    originalUrl: url.originalUrl,
+    shortCode: url.shortCode,
+    shortUrl: `${req.protocol}://${req.get("host")}/${url.shortCode}`,
+    clicks: url.clicks,
+    createdAt: url.createdAt,
+  });
+});
 
-    const url = await urlService.createShortUrl(originalUrl);
-    res.status(201).json({
-      originalUrl: url.originalUrl,
-      shortCode: url.shortCode,
-      shortUrl: `${req.protocol}://${req.get("host")}/${url.shortCode}`,
-      clicks: url.clicks,
-      // clickHistory: url.clickHistory,
-      createdAt: url.createdAt,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+export const redirectUrl = tryCatchFn(async (req, res, _next) => {
+  const { shortCode } = req.params;
+  const url = await urlService.getUrlByShortCode(shortCode);
+  res.redirect(301, url.originalUrl);
+});
 
-export const redirectUrl = async (req, res, next) => {
-  try {
-    const { shortCode } = req.params;
-    const url = await urlService.getUrlByShortCode(shortCode);
-    res.redirect(301, url.originalUrl);
-  } catch (error) {
-    next(error);
-  }
-};
+export const getUrlStats = tryCatchFn(async (req, res, _next) => {
+  const { shortCode } = req.params;
+  const url = await urlService.findUrlByShortCode(shortCode);
 
-export const getUrlStats = async (req, res, next) => {
-  try {
-    const { shortCode } = req.params;
-    const url = await urlService.findUrlByShortCode(shortCode);
+  const clicksByDay = url.clickHistory.reduce((acc, click) => {
+    const date = click.timestamp.toISOString().split("T")[0];
+    acc[date] = (acc[date] || 0) + 1;
+    return acc;
+  }, {});
 
-    const clicksByDay = url.clickHistory.reduce((acc, click) => {
-      const date = click.timestamp.toISOString().split("T")[0];
-      acc[date] = (acc[date] || 0) + 1;
-      return acc;
-    }, {});
+  res.status(200).json({
+    shortCode: url.shortCode,
+    originalUrl: url.originalUrl,
+    totalClicks: url.clicks,
+    clicksByDay,
+    createdAt: url.createdAt,
+    updatedAt: url.updatedAt,
+  });
+});
 
-    res.status(200).json({
-      shortCode: url.shortCode,
-      originalUrl: url.originalUrl,
-      totalClicks: url.clicks,
-      clicksByDay,
-      createdAt: url.createdAt,
-      updatedAt: url.updatedAt,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+export const shortenUrlWithValidation = [
+  validate(shortenUrlSchema),
+  shortenUrl,
+];
+
+// export const shortenUrlWithValidation = [validate(shortenUrlSchema), shortenUrl];
+// export const shortenUrlWithAuthAndValidation = [authMiddleware, validate(shortenUrlSchema), shortenUrl]; <- For protected routes
